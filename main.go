27f9b2607f567590a0ade389fa2cdf39af95701b
Fernando Rodriguez <fernando@nandotech.com>
2018-03-29 11:33:44 -0400

    refactor flagColor for repetition

--- main.go
+++ main.go
@@ -4,48 +4,56 @@ import (
 	"flag"
 	"fmt"
 	"image/color"
-	"log"
+
 	"strconv"
 )
 
-func main() {
-	background := flag.String("bg", "000000", "background color")
-	foreground := flag.String("fg", "000000", "foreground color")
-	flag.Parse()
-
-	fg, err := parseColor(*foreground)
-	if err != nil {
-		log.Fatal(err)
-	}
-	bg, err := parseColor(*background)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("background is %s\n", *background)
-
-	draw(fg, bg)
-
+type colorValue struct {
+	color.Color
 }
 
-func parseColor(s string) (color.Color, error) {
+func (c *colorValue) Set(s string) error {
 	v, err := strconv.ParseInt(s, 16, 64)
 	if err != nil {
-		log.Fatalf("background is not a color: %v", err)
+		return fmt.Errorf("background is not a color: %v", err)
 	}
 	b := uint8(v % 256)
 	g := uint8(v / 256 % 256)
 	r := uint8((v / (256 * 256)) % 256)
 
-	return color.RGBA{R: r, G: g, B: b, A: 255}, nil
+	c.Color = color.RGBA{R: r, G: g, B: b, A: 255}
+	return nil
 }
 
-func printColor(c color.Color) string {
-	r, g, b, a := c.RGBA()
-	r, g, b, a = r/256, g/256, b/256, a/256
+func (c *colorValue) String() string {
+	var r, g, b, a uint32
+	if c != nil && c.Color != nil {
+		r, g, b, a = c.RGBA()
+		r, g, b, a = r/256, g/256, b/256, a/256
+	}
+
 	return fmt.Sprintf("rgba(%v,%v,%v,%v)", r, g, b, a)
+}
+
+func flagColor(name string, value color.Color, usage string) color.Color {
+	v := &colorValue{value}
+	flag.Var(v, name, usage)
+	return v
+}
+func main() {
+	//fg, bg := colorValue{color.White}, colorValue{}
+	//flag.Var(&bg, "bg", "background color")
+	//flag.Var(&fg, "fg", "foreground color")
+	
+	fg := flagColor("fg",color.White,"foreground color")
+	bg := flagColor("bg", color.Black, "background color")
+	flag.Parse()
+
+	draw(fg, bg)
+
 }
 
 func draw(fg, bg color.Color) {
 	//r,g,b,a := c.RGBA()
-	fmt.Printf("drawing with foreground %s and background %s\n", printColor(fg), printColor(bg))
+	fmt.Printf("drawing with foreground %v and background %v\n", fg, bg)
 }
