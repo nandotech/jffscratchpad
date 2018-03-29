1b59c60a2039a8435a2081e9b884a88164406121
Fernando Rodriguez <fernando@nandotech.com>
2018-03-29 19:33:35 -0400

    added context and timeout from client

--- context-jff9/client/main.go
+++ context-jff9/client/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+	"context"
 	"os"
 	"io"
 	"log"
@@ -8,7 +10,18 @@ import (
 )
 
 func main() {
-	res, err := http.Get("localhost:8080")
+
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Second)
+	defer cancel()
+	
+	req, err := http.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req = req.WithContext(ctx)
+
+	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		log.Fatal(err)
 	}
