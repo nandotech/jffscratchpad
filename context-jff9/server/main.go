b659c3757c534d4d2e32d19e6e54344f2a53557c
Fernando Rodriguez <fernando@nandotech.com>
2018-03-29 20:01:53 -0400

    ensure requestID remains unique

--- context-jff9/server/main.go
+++ context-jff9/server/main.go
@@ -7,26 +7,26 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/campoy/justforfunc/context/log"
+	"github.com/nandotech/jffscratchpad/context-jff9/log"
 )
 
 func main() {
 	flag.Parse()
 	http.HandleFunc("/", log.Decorate(handler))
-	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
+	panic(http.ListenAndServe("127.0.0.1:8080", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	log.Printf("handler started")
-	defer log.Printf("handler ended")
+	log.Println(ctx, "handler started")
+	defer log.Println(ctx, "handler ended")
 
 	select {
 	case <-time.After(5 * time.Second):
 		fmt.Fprintln(w, "hello")
 	case <-ctx.Done():
 		err := ctx.Err()
-		log.Printf(err.Error())
+		log.Println(ctx, err.Error())
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 	fmt.Fprintln(w, "hello")
